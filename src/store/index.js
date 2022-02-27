6d8d932d6022010ddfe7f030cc2335bbe27802e4
Beattris <beatriceebirim@gmail.com>
2022-02-27 03:06:57 +0100

    installed redux toolkit and used the createSlice method to handle the state

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,29 @@
 import { createStore } from "redux";
+import { createSlice } from "@reduxjs/toolkit";
+
 
 const initialState = { counter: 0, showCounter: true};
+
+createSlice({
+  name: 'counter',
+  // writing initialState this way, makes JS to automatically make to initialState
+  // that is to : initialState = initialState
+  initialState,
+  reducers: {
+    increment(state){
+      state.counter++;
+    },
+    decrement(state){
+      state.counter--;
+    },
+    increase(state, action){
+      state.counter = state.counter + action.amount;
+    },
+    toggleCounter(state){
+      state.showCounter = !state.showCounter;
+    }
+  }
+})
 
 const counterReducer = (state = initialState, action) => {
   if (action.type === "INCREMENT") {
